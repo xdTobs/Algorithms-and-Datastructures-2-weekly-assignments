dd5551393f06047868407bf548021aba0a97acc2
tobia <tobias@schonau>
2023-09-17 20:09:32 +0200

    knapsack still not working

--- Kattis_Assignements/src/main/java/week3/Knapsack.java
+++ Kattis_Assignements/src/main/java/week3/Knapsack.java
@@ -6,76 +6,72 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Knapsack {
-    static List<KeyValue> input;
+    static List<Integer> inputWeights;
+    static List<Integer> inputValues;
+
+    static int[][] memo;
+
+    static List<Integer>[][] memoPaths;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<KnapsackPath> results = new ArrayList<>();
+        int results;
 
         do {
             int[] n = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            input = new ArrayList<>();
+            inputWeights = new ArrayList<>();
+            inputValues = new ArrayList<>();
             int weightCapacity = n[0];
             int inputSize = n[1];
+            memo = new int[inputSize+1][weightCapacity+1];
+            memoPaths = new ArrayList[inputSize+1][weightCapacity+1];
+            for(List<Integer>[] arr : memoPaths) Arrays.fill(arr, new ArrayList<>());
+            for(int[] arr : memo) Arrays.fill(arr, -1);
             for (int i = 0; i < inputSize; i++) {
                 int[] line = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                input.add(new KeyValue(line[1],line[0]));
+                inputValues.add(line[0]);
+                inputWeights.add(line[1]);
+
             }
-            results.add(knapsack(inputSize-1,weightCapacity));
+            results = (knapsack(inputSize-1,weightCapacity));
+            System.out.println(results);
+            //List<Integer> path = memoPaths[0][weightCapacity];
+            //System.out.println(path.size());
+            //System.out.println(path.stream().map(Objects::toString).collect(Collectors.joining(" ")));
+
 
         } while(br.ready());
 
-        for (KnapsackPath path : results){
-            System.out.println(path.totalCost);
-            for(KeyValue kv : path.indexes){
-                System.out.print(kv.value+ " ");
-            }
-            System.out.println();
-        }
-
     }
 
 
 
-    public static KnapsackPath knapsack(int index,int size){
-        if(index<0)
-            return new KnapsackPath(0);
-
-
-        if(size >= input.get(index).weight){
-            KnapsackPath p1 = knapsack(index-1, size);
-            KnapsackPath p2 = knapsack(index-1,size-input.get(index).weight);
-            if(p1.totalCost> p2.totalCost){
-                return p1;
-            }
-            else{
-                p2.indexes.add(input.get(index));
-                p2.totalCost+=p2.indexes.get(p2.indexes.size()-1).value;
-                return p2;
-            }
-
+    public static int knapsack(int index,int size){
+        if(index==0){
+            memo[index][size] = 0;
+            return memo[index][size];
         }
-        return knapsack(index-1, size);
+
+        if(memo[index][size] != -1) return memo[index][size];
+        if(size >= inputWeights.get(index)){
+            memo[index][size] = knapsack(index-1, size);
+            memoPaths[index][size] = new ArrayList<>(memoPaths[index - 1][size]);
+
+
+            memo[index][size-inputWeights.get(index)] = knapsack(index-1, size-inputWeights.get(index))+inputValues.get(index);
+            memoPaths[index][size - inputWeights.get(index)] = new ArrayList<>(memoPaths[index - 1][size - inputWeights.get(index)]);
+            memoPaths[index][size - inputWeights.get(index)].add(index);
+            int opt1 = memo[index][size];
+            int opt2 = memo[index][size-inputWeights.get(index)];
+
+            return Math.max(opt1,opt2);
+        }
+        memo[index][size] = knapsack(index-1, size);
+        memoPaths[index][size] = new ArrayList<>(memoPaths[index - 1][size]);
+        return memo[index][size];
     }
 
-    static class KeyValue{
-        public int weight;
-        public int value;
-
-        public KeyValue(int weight, int value) {
-            this.weight = weight;
-            this.value = value;
-        }
-    }
-    static class KnapsackPath{
-        int totalCost;
-
-        List<KeyValue>indexes;
-
-        public KnapsackPath(int totalCost) {
-            this.totalCost = totalCost;
-            this.indexes = new ArrayList<>();
-        }
-    }
 }
